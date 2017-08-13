21380eb189ab8999f08d9b59a41a0b6e2e64cf23
Christoph Reimers <christoph@superservice-international.com>
2017-08-13 11:33:06 +0200

    add shell script

---
+++ ddns.sh
@@ -0,0 +1,6 @@
+export ZONE_ID="your zone id"
+export RECORD_ID="your record id"
+export USER_EMAIL="your email"
+export API_KEY="your api key"
+
+/home/pi/cloudflare-ddns/venv/bin/python /home/pi/cloudflare-ddns/ddns.py
